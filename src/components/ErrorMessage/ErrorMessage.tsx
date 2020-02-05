08b52dd3badf385efbc7174b68decf39c110d4ac
Rezwanul-Haque <rezwanul.cse@gmail.com>
2020-02-05 22:08:07 +0600

    Initial commit

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import styled from "styled-components";
+
+/** Theme */
+import { Colors } from "../../Theme";
+
+type ErrorMessageProps = {
+  errorMessage: Error | string;
+};
+
+const ErrorMessage = styled.p`
+  text-align: center;
+  margin-top: 10px;
+  color: rgba(200, 0, 0, 0.8);
+`;
+
+const ErrorMessageContainer: React.FC<ErrorMessageProps> = ({
+  errorMessage,
+}) => {
+  return <ErrorMessage>{errorMessage}</ErrorMessage>;
+};
+
+export default ErrorMessageContainer;
